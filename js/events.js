685d14467f281e6645467fe17de8e62c275ea6b9
Ahmadali <ahmadali.azizov@outlook.com>
2018-04-30 15:28:50 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -19,7 +19,7 @@ function pressIt () {
 }
 
 function submitIt () {
-  $('input:last').on('submit', function() {
+  $('form').on('submit', function() {
     alert ('Your form is going to be submitted now.');
   });
 }
